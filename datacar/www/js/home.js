b82743cb6b7026ab392f72d6ed14dcf42b8715e1
Yago-Angoera <62061468+Yago-Angoera@users.noreply.github.com>
2022-03-14 12:47:07 -0300

    Navegação para tela de resultados

---
+++ datacar/www/js/home.js
@@ -0,0 +1,4 @@
+document.getElementById("bt-search").addEventListener("click", function( event ) {
+    // mostra o contador de cliques dentro da div clicada
+    window.location.href="result.html";
+  });
